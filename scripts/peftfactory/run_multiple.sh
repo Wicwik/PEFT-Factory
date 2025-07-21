f41e7d7d5daf455d130935ed3b6f7db150f590d0
Robert Belanec <belanecrobert22@gmail.com>
2025-07-21 10:33:40 +0000

    run others

--- scripts/peftfactory/run_multiple.sh
+++ scripts/peftfactory/run_multiple.sh
@@ -23,8 +23,8 @@
 # peft_methods=(ia3 prompt-tuning lora lntuning)
 # models=(gemma-3-1b-it llama-3-8b-instruct mistral-7b-instruct)
 
-datasets=(mnli)
-peft_methods=(prompt-tuning)
+datasets=(mnli qqp qnli sst2 stsb mrpc rte cola record multirc boolq wic wsc cb copa)
+peft_methods=(ia3 lora lntuning)
 models=(llama-3-8b-instruct)
 
 export DISABLE_VERSION_CHECK=1
