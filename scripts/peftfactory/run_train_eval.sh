56ffe955b0349c79584106595735936537c82f95
Róbert Belanec <belanecrobert22@gmail.com>
2025-10-13 11:05:44 +0200

    run eval script

---
+++ scripts/peftfactory/run_train_eval.sh
@@ -0,0 +1,43 @@
+datasets=(wsc)
+# peft_methods=(prefix-tuning prompt-tuning p-tuning lora lntuning ia3)
+peft_methods=(prompt-tuning)
+models=(llama-3-8b-instruct)
+seeds=(42 123 456 789 101112)
+EPOCHS=20
+
+for s in ${seeds[@]};
+do
+    for d in ${datasets[@]};
+    do
+        for m in ${models[@]};
+        do
+            for pm in ${peft_methods[@]};
+            do
+                TIMESTAMP=`date +%s`
+                OUTPUT_DIR="saves/${pm}/${m}/train_${d}_${s}_${TIMESTAMP}"
+                DATASET="${d}"
+                SEED="${s}"
+                WANDB_PROJECT="peft-factory-${pm}"
+                WANDB_NAME="${pm}_${m}_train_${d}_${s}_${TIMESTAMP}"
+
+
+                mkdir -p ${OUTPUT_DIR}
+
+                export OUTPUT_DIR DATASET SEED WANDB_PROJECT WANDB_NAME EPOCHS
+                envsubst < examples/peft/${pm}/${m}/train.yaml > ${OUTPUT_DIR}/train.yaml
+
+                OUTPUT_DIR="saves/${pm}/${m}/eval_${d}_${s}_${TIMESTAMP}"
+                WANDB_NAME="${pm}_${m}_eval_${d}_${s}_${TIMESTAMP}"
+                ADAPTER="saves/${pm}/${m}/train_${d}_${s}_${TIMESTAMP}"
+
+                mkdir -p ${OUTPUT_DIR}
+
+                export OUTPUT_DIR WANDB_NAME ADAPTER
+                envsubst < examples/peft/${pm}/${m}/eval.yaml > ${OUTPUT_DIR}/eval.yaml
+
+                llamafactory-cli train saves/${pm}/${m}/train_${d}_${s}_${TIMESTAMP}/train.yaml
+                llamafactory-cli train saves/${pm}/${m}/eval_${d}_${s}_${TIMESTAMP}/eval.yaml
+                python scripts/peftfactory/compute_metrics.py saves/${pm}/${m}/eval_${d}_${s}_${TIMESTAMP} ${d}
+        done
+    done
+done
